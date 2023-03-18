4abf3dbdbf32cd89e4bbbe6c55f673702ba96e6a
Italo P. Brito <pbitalo@hotmail.com>
2023-03-18 19:22:53 -0300

    change name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'sys-zord'`, () => {
+  it(`should have as title 'clean-arq-with-angular-io'`, () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
-    expect(app.title).toEqual('sys-zord')
+    expect(app.title).toEqual('clean-arq-with-angular-io')
   })
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'sys-zord app is running!'
+      'clean-arq-with-angular-io app is running!'
     )
   })
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sys-zord'
+  title = 'clean-arq-with-angular-io'
 }
